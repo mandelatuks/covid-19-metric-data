636fdad8f09dec36d087e9ea2cd6f1c07bca8f03
mandelatuks <manderat206@gmail.com>
2022-06-22 16:36:32 +0200

    Add functionality to home

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { Link, useNavigate, useLocation } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { FaSearchLocation } from "react-icons/fa";
-import fetchData from "../redux/covid/Api";
-import { fetchStats } from "../redux/covid/reducer";
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { FaSearchLocation } from 'react-icons/fa';
+import fetchData from '../redux/covid/Api';
+import { fetchStats } from '../redux/covid/reducer';
 
 const HomePage = () => {
   const countryStore = useSelector((store) => store.details);
@@ -15,23 +15,62 @@ const HomePage = () => {
     }
   }, [countryStore.length, dispatch]);
 
-  let continCovid = countryStore.filter((item) => item.continent === "Europe");
+  let continCovid = countryStore.filter((item) => item.continent === 'Europe');
   const location = useLocation();
   const query = new URLSearchParams(location.search);
-  const search = query.get("search") || "";
-  continCovid = continCovid.filter((country) =>
-    country.country.includes(search)
-  );
+  const search = query.get('search') || '';
+  continCovid = continCovid.filter((country) => country.country.includes(search));
   const navigate = useNavigate();
   const [searchValue, setSearchValue] = useState(search);
 
   const countryFilterOnChange = (event) => {
-    navigate(event.target.value ? `?search=${event.target.value}` : "");
+    navigate(event.target.value ? `?search=${event.target.value}` : '');
     setSearchValue(event.target.value);
   };
 
   return (
-   
+    <div className="pages">
+      <h1 className="area"> Europe</h1>
+      <form className="form">
+        <FaSearchLocation />
+        <div>
+          <input
+            className="input-area"
+            type="text"
+            value={searchValue}
+            placeholder="Search Here"
+            onChange={countryFilterOnChange}
+          />
+        </div>
+      </form>
+      <ul className="list-country">
+        {continCovid.map((country) => (
+          <Link
+            key={country.country}
+            to={{ pathname: `/country/${country.country}` }}
+          >
+            <li className="list-details">
+              <div className=" details">
+                <h1 className="name">{country.country}</h1>
+              </div>
+              <div>
+                <h2 className="pupalution">Population:</h2>
+                {' '}
+                <p className="number">{country.population.toLocaleString()}</p>
+              </div>
+              <div className="photo">
+                <img
+                  src={country.country_flag}
+                  alt="national flag"
+                  className="national-flag"
+                />
+              </div>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default HomePage;
